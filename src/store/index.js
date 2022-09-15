9c9e9a6edada314ee87c235e996404b50d462079
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2022-09-16 02:54:58 +0300

    feat: add main interface

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,239 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    categories: [{
+        id: 0,
+        title: "Обязательные для всех",
+        subtitle: "Документы, обязательные для всех сотрудников без исключения",
+        markers: [{
+            background: "#FF238D",
+            outline: false
+          },
+          {
+            background: "#FFB800",
+            outline: false
+          },
+          {
+            background: "#FF8D23",
+            outline: true
+          },
+        ],
+        type: null,
+        elements: [{
+            id: 0,
+            title: "Паспорт",
+            subtitle: "Для всех",
+            markers: [{
+              background: "#00C2FF",
+              outline: false
+            }],
+            type: "Обязательный",
+          },
+          {
+            id: 1,
+            title: "ИНН",
+            subtitle: "Для всех",
+            markers: [],
+            type: "Обязательный",
+          },
+        ],
+      },
+      {
+        id: 1,
+        title: "Обязательные для трудоустройства",
+        subtitle: "Документы, без которых невозможно трудоустройство человека на какую бы то ни было должность в компании вне зависимости от граж",
+        markers: [],
+        type: null,
+        elements: [],
+      },
+      {
+        id: 2,
+        title: "Специальные",
+        subtitle: null,
+        markers: [],
+        type: null,
+        elements: [],
+      },
+    ],
+    elements: [{
+        id: 2,
+        title: "Тестовое задание кандидата",
+        subtitle: "Россия, Белоруссия, Украина, администратор филиала, повар-сушист, повар-пиццмейкер, повар горячего цеха",
+        markers: [],
+        type: null,
+      },
+      {
+        id: 3,
+        title: "Трудовой договор",
+        subtitle: null,
+        markers: [{
+            background: "#0066FF",
+            outline: false
+          },
+          {
+            background: "#8E9CBB",
+            outline: false
+          }
+        ],
+        type: null,
+      },
+      {
+        id: 4,
+        title: "Мед. книжка",
+        subtitle: null,
+        markers: [],
+        type: null,
+      },
+    ],
+    buttons: [{
+        name: "edit",
+        alt: "edit button icon"
+      },
+      {
+        name: "delete",
+        alt: "delete button icon"
+      },
+      {
+        name: "moving",
+        alt: "moving button icon"
+      },
+    ],
+    search: null,
+    filterCategories: [],
+    filterElements: [],
+    activeElement: null,
+    currentElement: null,
   },
   mutations: {
+    changeSearch(state, text) {
+      state.search = text;
+    },
+    changefilterCategories(state) {
+      let copyCategories = state.categories.slice();
+
+      if (state.search !== null) {
+        copyCategories = copyCategories.filter(
+          (doc) =>
+          doc.title.toLowerCase().indexOf(state.search.toLowerCase()) > -1
+        );
+
+        copyCategories.forEach(category => {
+          category.elements = category.elements.filter((elem) => elem.title.toLowerCase().indexOf(state.search.toLowerCase()) > -1);
+        })
+      }
+      state.filterCategories = copyCategories;
+    },
+    changeFilterElements(state) {
+      let copyElements = state.elements.slice();
+
+      if (state.search !== null) {
+        copyElements = copyElements.filter(
+          (elem) =>
+          elem.title.toLowerCase().indexOf(state.search.toLowerCase()) > -1
+        );
+      }
+
+      state.filterElements = copyElements;
+    },
+    insertCategory(state, payload) {
+      const indexActiveElement = state.filterCategories.findIndex(el => el.id === payload.active);
+      const indexCurrentElement = state.filterCategories.findIndex(el => el.id === payload.current);
+      const deleteCategory = state.filterCategories.slice(0)[indexActiveElement];
+
+      if (indexActiveElement > -1 && indexCurrentElement > -1) {
+        state.filterCategories.splice(indexActiveElement, 1);
+
+        setTimeout(() => {
+          state.filterCategories.splice(indexCurrentElement, 0, deleteCategory);
+        }, 1000);
+      }
+
+    },
+    changeActiveElement(state, payload) {
+      state.activeElement = payload;
+    },
+    changeCurrentElement(state, payload) {
+      state.currentElement = payload;
+    },
+    changeElements(state, payload) {
+      let deleteElement = null;
+
+      if (payload.active.category === null) {
+        deleteElement = state.filterElements.slice(0)[payload.active.index];
+        state.filterElements.splice(payload.active.index, 1);
+      } else {
+        deleteElement = state.filterCategories[payload.active.category].elements.slice(0)[payload.active.index];
+        state.filterCategories[payload.active.category].elements.splice(payload.active.index, 1);
+      }
+
+      if (payload.current.category === null) {
+        setTimeout(() => {
+          state.filterElements.splice(payload.current.index, 0, deleteElement);
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          state.filterCategories[payload.current.category].elements.splice(payload.current.index, 0, deleteElement);
+        }, 1000);
+      }
+    },
+  },
+  getters: {
+    docsCategories(state) {
+      return state.filterCategories;
+    },
+    docsElements(state) {
+      return state.filterElements;
+    },
+    docsButtons(state) {
+      return state.buttons;
+    },
   },
   actions: {
+    defineIndexElement({
+      state,
+      commit
+    }, payload) {
+      let indexElement = state.filterElements.findIndex(el => el.id === payload.id);
+      let element = null;
+
+      if (indexElement > -1) {
+        element = {
+          category: null,
+          index: indexElement,
+        };
+      } else {
+        state.filterCategories.forEach((category, index) => {
+          indexElement = category.elements.findIndex(el => el.id === payload.id);
+          if (indexElement > -1) {
+            element = {
+              category: index,
+              index: indexElement,
+            };
+          }
+        })
+      }
+
+      commit(payload.mutation, element);
+    },
+    insertElements({
+      state,
+      dispatch,
+      commit
+    }, payload) {
+      dispatch("defineIndexElement", {
+        id: payload.active,
+        mutation: 'changeActiveElement'
+      });
+      dispatch("defineIndexElement", {
+        id: payload.current,
+        mutation: 'changeCurrentElement'
+      });
+
+      commit("changeElements", {
+        active: state.activeElement,
+        current: state.currentElement
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -2,18 +2,274 @@
   <div class="docs">
     <h2 class="docs__title">Документы</h2>
     <BaseSearch :search.sync="docsSearch" />
+
+    <v-expansion-panels ref="listCategories" accordion class="docs__categories">
+      <TransitionGroup name="list">
+        <v-expansion-panel
+          v-for="(category, indexCategory) in docsCategories"
+          :key="category.id"
+          @click="indexCategoryActive = indexCategory"
+          class="docs__category"
+        >
+          <v-expansion-panel-header
+            v-slot="{ open }"
+            class="docs__header"
+            :class="{
+              docs__header_next: checkCategory(indexCategory),
+            }"
+          >
+            <DocCategory
+              :ref="`category-${category.id}`"
+              :category="category"
+              :open="open"
+              @move="dragMoveCategory($event)"
+              @end="dragEndCategory($event)"
+              @start="currentDrapCategory = category"
+            />
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <div class="docs__elements">
+              <DocElement
+                v-for="element in category.elements"
+                :key="element.id"
+                :ref="`element-${element.id}`"
+                :element="element"
+                @move="dragMoveElement($event)"
+                @end="dragEndElement($event)"
+                @start="currentDrapElement = element"
+              />
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </TransitionGroup>
+    </v-expansion-panels>
+
+    <div ref="listElements" class="docs__elements">
+      <TransitionGroup name="list">
+        <DocElement
+          v-for="element in docsElements"
+          :key="element.id"
+          :ref="`element-${element.id}`"
+          :element="element"
+          @move="dragMoveElement($event)"
+          @end="dragEndElement($event)"
+          @start="currentDrapElement = element"
+        />
+      </TransitionGroup>
+    </div>
+
+    <span
+      v-if="docsCategories.length === 0 && docsElements.length === 0"
+      class="docs__error"
+    >
+      Ничего не найдено. Попробуйте изменить параметры поиска.
+    </span>
+    <div ref="dragElement" class="docs__drop">
+      <DocElement :element="currentDrapElement" :drop="true" />
+    </div>
+    <div ref="dragCategory" class="docs__drop">
+      <DocCategory :category="currentDrapCategory" :drop="true" />
+    </div>
   </div>
 </template>
 
 <script>
 import BaseSearch from "@/components/BaseSearch.vue";
+import DocCategory from "@/components/DocCategory.vue";
+import DocElement from "@/components/DocElement.vue";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 
 export default {
-  components: { BaseSearch },
+  components: { BaseSearch, DocCategory, DocElement },
   data() {
     return {
       docsSearch: null,
+      indexCategoryActive: null,
+      currentDrapCategory: null,
+      currentDrapElement: null,
+      payloadCategories: null,
+      payloadElements: null,
     };
+  },
+  watch: {
+    docsSearch() {
+      this.changeSearch(this.docsSearch);
+      this.changefilterCategories();
+      this.changeFilterElements();
+    },
+  },
+  computed: {
+    ...mapGetters(["docsCategories", "docsElements"]),
+  },
+  methods: {
+    ...mapMutations([
+      "changeSearch",
+      "changefilterCategories",
+      "changeFilterElements",
+      "insertCategory",
+    ]),
+    ...mapActions(["insertElements"]),
+    checkCategory(index) {
+      return (
+        this.indexCategoryActive !== null &&
+        index === this.indexCategoryActive + 1
+      );
+    },
+    dragMoveCategory(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      const container = this.$refs.listCategories.$el.getBoundingClientRect();
+
+      this.$refs.dragCategory.hidden = true;
+      const elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+      this.$refs.dragCategory.hidden = false;
+
+      const activeElement =
+        this.$refs[`category-${this.currentDrapCategory.id}`];
+
+      this.deleteClassInsert();
+      this.payloadCategories = null;
+
+      if (
+        elemBelow &&
+        activeElement[0].$el !== elemBelow &&
+        elemBelow.classList.contains("category")
+      ) {
+        elemBelow.classList.add("category_insert");
+        let idCurrentElement;
+
+        this.docsCategories.forEach((elem) => {
+          if (elemBelow === this.$refs[`category-${elem.id}`][0].$el) {
+            idCurrentElement = elem.id;
+          }
+        });
+
+        this.payloadCategories = {
+          active: this.currentDrapCategory.id,
+          current: idCurrentElement,
+        };
+      }
+
+      if (
+        e.clientY >= container.top &&
+        e.clientY <= container.top + container.height
+      )
+        this.$refs.dragCategory.style.top = e.clientY + "px";
+      this.$refs.dragCategory.style.left = "30px";
+    },
+    dragEndCategory(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      if (this.payloadCategories !== null)
+        this.insertCategory(this.payloadCategories);
+
+      this.$refs.dragCategory.style.top = "-100vh";
+      this.deleteClassInsert();
+    },
+    dragMoveElement(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      const containerCategories =
+        this.$refs.listCategories.$el.getBoundingClientRect();
+      const containerElements = this.$refs.listElements.getBoundingClientRect();
+
+      this.$refs.dragElement.hidden = true;
+      const elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+      this.$refs.dragElement.hidden = false;
+
+      const activeElement = this.$refs[`element-${this.currentDrapElement.id}`];
+
+      this.deleteClassInsertForElement();
+      this.payloadElements = null;
+
+      if (
+        elemBelow &&
+        activeElement[0].$el &&
+        activeElement[0].$el !== elemBelow &&
+        elemBelow.classList.contains("element")
+      ) {
+        elemBelow.classList.add("element_insert");
+        let idCurrentElement = null;
+
+        this.docsElements.forEach((elem) => {
+          if (elemBelow === this.$refs[`element-${elem.id}`][0].$el) {
+            idCurrentElement = elem.id;
+          }
+        });
+
+        if (idCurrentElement === null) {
+          this.docsCategories.forEach((category) => {
+            if (category.elements.length > 0) {
+              category.elements.forEach((elem) => {
+                if (
+                  this.$refs[`element-${elem.id}`] &&
+                  elemBelow === this.$refs[`element-${elem.id}`][0].$el
+                ) {
+                  idCurrentElement = elem.id;
+                }
+              });
+            }
+          });
+        }
+
+        this.payloadElements = {
+          active: this.currentDrapElement.id,
+          current: idCurrentElement,
+        };
+      }
+
+      if (
+        e.clientY >= containerCategories.top &&
+        e.clientY <= containerElements.top + containerElements.height
+      )
+        this.$refs.dragElement.style.top = e.clientY + "px";
+      this.$refs.dragElement.style.left = "30px";
+    },
+    dragEndElement(e) {
+      e = e || window.event;
+      e.preventDefault();
+
+      if (this.payloadElements !== null)
+        this.insertElements(this.payloadElements);
+
+      this.$refs.dragElement.style.top = "-100vh";
+      this.deleteClassInsertForElement();
+    },
+    deleteClassInsert() {
+      const categories =
+        this.$refs.listCategories.$el.querySelectorAll(".category");
+      if (categories) {
+        categories.forEach((category) => {
+          if (category.classList.contains("category_insert"))
+            category.classList.remove("category_insert");
+        });
+      }
+    },
+    deleteClassInsertForElement() {
+      const elementsCategories =
+        this.$refs.listCategories.$el.querySelectorAll(".element");
+      const elements = this.$refs.listElements.querySelectorAll(".element");
+
+      if (elementsCategories) {
+        elementsCategories.forEach((element) => {
+          if (element.classList.contains("element_insert"))
+            element.classList.remove("element_insert");
+        });
+      }
+      if (elements) {
+        elements.forEach((element) => {
+          if (element.classList.contains("element_insert"))
+            element.classList.remove("element_insert");
+        });
+      }
+    },
+  },
+  mounted() {
+    this.changefilterCategories();
+    this.changeFilterElements();
   },
 };
 </script>
@@ -25,6 +281,10 @@ export default {
   margin: 30px;
   font-family: "Fira Sans", sans-serif;
 
+  &_dragging {
+    opacity: 0.2;
+  }
+
   &__title {
     margin-top: 3px;
     margin-bottom: 15px;
@@ -32,5 +292,164 @@ export default {
     font-size: 22px;
     color: black;
   }
+
+  &__elements {
+    margin-top: 14px;
+  }
+
+  &__categories {
+    margin-top: 18px;
+    border-radius: 0;
+
+    & > *:first-child {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+      width: 100%;
+    }
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+    border: 1px solid #dfe4ef;
+  }
+
+  &__category {
+    width: 100%;
+    min-width: 100%;
+
+    &::before {
+      border-radius: 0;
+      box-shadow: none;
+    }
+
+    &:not(:first-child)::after {
+      border-top: none;
+    }
+
+    &:not(:first-child) {
+      .docs__header {
+        border-top: 0;
+
+        &_next {
+          border: 1px solid #dfe4ef;
+        }
+      }
+    }
+
+    .v-expansion-panel-header__icon {
+      display: none;
+    }
+
+    .v-expansion-panel-content__wrap {
+      padding: 0 0 0 16px;
+
+      .element {
+        border-top: 0;
+
+        &:last-child {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    &.v-expansion-panel--active > .v-expansion-panel-header {
+      min-height: auto;
+    }
+
+    &.v-expansion-panel--active + .v-expansion-panel {
+      margin-top: 0;
+    }
+
+    .docs__elements {
+      margin-top: 0;
+    }
+  }
+
+  &__content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  &__info {
+    display: flex;
+    align-items: center;
+  }
+
+  &__markers {
+    display: flex;
+    margin-left: 16px;
+  }
+
+  &__marker {
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+
+    &:not(:first-child) {
+      margin-left: 6px;
+    }
+
+    &_outline {
+      border: 1px solid #000000;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+  }
+
+  &__type,
+  &__subtitle {
+    margin-left: 15px;
+    font-weight: 400;
+    font-size: 11px;
+  }
+
+  &__type {
+    color: #ff238d;
+  }
+
+  &__subtitle {
+    color: #8e9cbb;
+  }
+
+  &__buttons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 75px;
+    max-width: 75px;
+    min-width: 75px;
+  }
+
+  &__drop {
+    position: absolute;
+    top: -100vh;
+    z-index: 1;
+    width: calc(100% - 60px);
+    background: white;
+    border: 1px solid #dfe4ef;
+    box-shadow: 0px 3px 16px rgba(0, 102, 255, 0.7);
+
+    .element {
+      border: none;
+    }
+  }
+
+  &__error {
+    color: #8e9cbb;
+  }
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
